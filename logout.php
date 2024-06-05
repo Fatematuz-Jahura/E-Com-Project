6ba1b149092c89e1c377bacd52b59d82ef4bbf31
Fatematuz-Jahura <83083159+Fatematuz-Jahura@users.noreply.github.com>
2024-06-06 04:22:41 +0600

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,9 +1,7 @@
 <?php
-require('connection.inc.php');
-require('functions.inc.php');
-unset($_SESSION['USER_LOGIN']);
-unset($_SESSION['USER_ID']);
-unset($_SESSION['USER_NAME']);
-header('location:index.php');
+session_start();
+unset($_SESSION['ADMIN_LOGIN']);
+unset($_SESSION['ADMIN_USERNAME']);
+header('location:login.php');
 die();
 ?>
